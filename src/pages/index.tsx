802dcecef2ed0c3ed0332f2796e5f8ce8688ea31
jackvdp <jack@pumpymusic.co.uk>
2024-11-15 22:02:44 +0000

    Move theme provider to app file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ import { TasksTable } from "@/components/tasks-table";
 import { useState } from "react"
 import { SponsorsProvider } from "@/hooks/use-sponsors"
 import { TasksProvider } from "@/hooks/use-tasks";
-import { ThemeProvider } from "@/components/theme-provider";
 
 type View = 'sponsors' | 'tasks';
 
@@ -14,32 +13,25 @@ export default function DashboardPage() {
   const [currentView, setCurrentView] = useState<View>('tasks');
 
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <TasksProvider>
-        <SponsorsProvider>
-          <SidebarProvider defaultOpen>
-            <div className="flex min-h-screen w-full">
-              <DashboardSidebar
-                currentView={currentView}
-                onViewChange={setCurrentView}
-              />
-              <main className="flex-1 px-6 pb-6">
-                {currentView === 'sponsors' &&
-                  <SponsorsTable />
-                }
-                {currentView === 'tasks' &&
-                  <TasksTable />
-                }
-              </main>
-            </div>
-          </SidebarProvider>
-        </SponsorsProvider>
-      </TasksProvider>
-    </ThemeProvider>
+    <TasksProvider>
+      <SponsorsProvider>
+        <SidebarProvider defaultOpen>
+          <div className="flex min-h-screen w-full">
+            <DashboardSidebar
+              currentView={currentView}
+              onViewChange={setCurrentView}
+            />
+            <main className="flex-1 px-6 pb-6">
+              {currentView === 'sponsors' &&
+                <SponsorsTable />
+              }
+              {currentView === 'tasks' &&
+                <TasksTable />
+              }
+            </main>
+          </div>
+        </SidebarProvider>
+      </SponsorsProvider>
+    </TasksProvider>
   );
 }
